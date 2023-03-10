1ae8d2a55dd73549d2f079a838f37eb64a0da8c3
akash7293 <akashkothawade77@gmail.com>
2023-03-10 22:20:47 +0530

    shimmer issue for menu page fixed

--- Chapter-04-Talk is Cheap, Show me the code/src/components/RestaurantMenu.js
+++ Chapter-04-Talk is Cheap, Show me the code/src/components/RestaurantMenu.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 import { IMG_CDN_URL } from '../../config';
+import ShimmerUi from './ShimmerUi';
 
 const RestaurantMenu = () => {
 	const { resId } = useParams();
-	const [restaurant, setRestaurant] = useState([]);
+	const [restaurant, setRestaurant] = useState(null);
 	const [items, setItems] = useState({});
 
 	useEffect(() => {
@@ -26,16 +27,18 @@ const RestaurantMenu = () => {
 			json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
 				?.card,
 		);
-		console.log(
-			json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
-				?.card,
-		);
+		// console.log(
+		// 	json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+		// 		?.card,
+		// );
 		// const resName = setRestaurant(json?.data?.cards[0]?.card?.card?.info?.name);
 		// const resImg =
 		// 	IMG_CDN_URL + json?.data?.cards[0]?.card?.card?.info?.cloudinaryImageId;
 	}
 
-	return (
+	return !restaurant ? (
+		<ShimmerUi />
+	) : (
 		<div>
 			<h1>{restaurant.name}</h1>
 			<img src={IMG_CDN_URL + restaurant.cloudinaryImageId} />
